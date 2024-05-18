cff303d119468b9cb4ae67c917c271ab65f38bc6
GAUTAM JHA <gautamjha.mailbox@gmail.com>
2024-05-18 19:36:35 +0530

    Fixed: Minor Issues

--- packages/mfe/src/App.vue
+++ packages/mfe/src/App.vue
@@ -1,19 +1,31 @@
 <template>
   <h2>Micro FE App</h2>
+  {{ JSON.stringify(data) }}
 </template>
 
 <script setup>
-try{
-  const data1  = await import(`${window.versionPrefix}/config.js?url`)
-  console.log({data1})
-}catch(e) {
-  console.warn(e)
-}
+import { ref } from "vue"
+const data = ref({
+   config1: null,
+   config2: null,
+})
 
-try{
-  const data2  = await import('../config.js?url')
-  console.log({data2})
-}catch(e) {
-  console.warn('e2',e)
+const resolveConfig = async () => {
+    try{
+      const config1  = await import(`${window.versionPrefix}/config.js?url`)
+      data.value.config1  = data1;
+      console.log({data1})
+    }catch(e) {
+      console.warn(e)
+    }
+
+    try{
+      const config2  = await import('../config.js?url')
+      console.log({config2})
+      data.value.config2  = config2;
+    }catch(e) {
+      console.warn('e2',e)
+    }
 }
+resolveConfig()
 </script>
